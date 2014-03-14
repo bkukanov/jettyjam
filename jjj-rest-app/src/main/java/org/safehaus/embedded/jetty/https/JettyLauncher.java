a0f2e8af959330edbc5cc09cea9f05d31e2b3c57
Alex Karasulu <akarasulu@gmail.com>
2014-03-14 12:57:46 +0200

    added new skeleton for jersey, jackson, and jetty example with guice

---
+++ jjj-rest-app/src/main/java/org/safehaus/embedded/jetty/https/JettyLauncher.java
@@ -0,0 +1,32 @@
+package org.safehaus.embedded.jetty.https;
+
+
+import org.safehaus.embedded.jetty.utils.JettyHandlers;
+import org.safehaus.embedded.jetty.utils.ServletMapping;
+
+
+/**
+ * A simple JettyLauncher.
+ */
+@JettyHandlers(
+    servletMappings = { @ServletMapping( servlet = HelloWorldServlet.class, spec = "/*" ) },
+    filterMappings = {}
+)
+public class JettyLauncher extends org.safehaus.embedded.jetty.utils.Launcher {
+
+    public JettyLauncher() {
+        super( "TestApp", JettyLauncher.class.getClassLoader() );
+    }
+
+
+    @Override
+    public String getPackageBase() {
+        return getClass().getPackage().getName();
+    }
+
+
+    public static void main( String [] args ) throws Exception {
+        JettyLauncher launcher = new JettyLauncher();
+        launcher.start();
+    }
+}
