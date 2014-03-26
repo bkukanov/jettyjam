d91fa22743e6b73cbefcbd8ba3821bf83b804b12
Alex Karasulu <akarasulu@gmail.com>
2014-03-26 21:22:27 +0200

    cleaning up old class loaders used

--- utils/src/main/java/org/safehaus/embedded/jetty/utils/Foo.java
+++
@@ -1,8 +0,0 @@
-package org.safehaus.embedded.jetty.utils;
-
-
-/**
- * Created with IntelliJ IDEA. User: akarasulu Date: 3/26/14 Time: 8:32 PM To change this template use File | Settings |
- * File Templates.
- */
-public class Foo {}
